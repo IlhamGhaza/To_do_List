53978ff08d38358d7775ac362dedc0c16f4d9497
Ghazali <m.ilhamghazali@gmail.com>
2023-08-24 11:57:43 +0700

    Initial commit

---
+++ To_do_list/src/App.java
@@ -0,0 +1,33 @@
+public class App {
+
+    public static String[] model =new String[10];
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    //menampilkan to do list
+    public static void showTodoList(){
+
+    }
+
+    //menambah to do list
+    public static void addTodoList(){
+
+    }
+    //remove to do list
+    public static void removeTodoList(){
+    }
+
+    //menampilkan view to do list
+    public static void viewShowTodoList(){
+
+    }
+    
+    public static void viewAddTodoList(){
+
+    }
+    public static void viewRemoveTodoList(){
+
+    }
+}
